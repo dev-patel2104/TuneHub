3299906545392dc5bdf8a772a973212f4d2dfd47
Harsh Pranav Patel <hr979846@dal.ca>
2023-06-19 16:45:24 -0300

    added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,119 @@
-import { Box, Flex, HStack, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Text, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, IconButton, useDisclosure, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import { HamburgerIcon } from '@chakra-ui/icons';
+
 
 function NavBar() {
-  return (
-    <Flex as="nav" alignItems="center" justify="space-between">
-      {/* Logo */}
-      <NavLink to="/">
-        <Box padding="16px 24px">
-          <Text>TuneHub</Text>
-        </Box>
-      </NavLink>
+  const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-      <HStack>
-        {/* Header */}
-        <Box padding="16px 24px">
+  return (
+    isMobile ?
+      <Flex as="nav" alignItems="center" justify="space-between" h="10vh" w="100%" backgroundColor="#050A30">
+        {/* Logo */}
+        <NavLink to="/">
+          <Text fontSize="sm" color="white" ml="8px">TuneHub</Text>
+        </NavLink>
+        <Box>
+          <IconButton
+            icon={<HamburgerIcon color="white" />}
+            variant="ghost"
+            onClick={onOpen}
+            aria-label="Open Menu"
+            mr="16px"
+          />
+
+          <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+            <DrawerOverlay/>
+            <DrawerContent backgroundColor="#050A30">
+              <DrawerCloseButton color="white"/>
+              <DrawerHeader color="white" align="center">TuneHub</DrawerHeader>
+              <DrawerBody>
+                <VStack gap="12px">
+                  {/* Home */}
+                  <Box>
+                    <NavLink to='/'>
+                      <Text fontWeight="medium" color="white">Home</Text>
+                    </NavLink>
+                  </Box>
+                  {/* Features */}
+                  <Box>
+                    <NavLink to='/'>
+                      <Text fontWeight="medium" color="white">Features</Text>
+                    </NavLink>
+                  </Box>
+                  {/* About Us */}
+                  <Box>
+                    <NavLink to='/'>
+                      <Text fontWeight="medium" color="white">About Us</Text>
+                    </NavLink>
+                  </Box>
+                  {/* News */}
+                  <Box>
+                    <NavLink to='/'>
+                      <Text fontWeight="medium" color="white">News</Text>
+                    </NavLink>
+                  </Box>
+                  {/* Contact */}
+                  <Box>
+                    <NavLink to='/'>
+                      <Text fontWeight="medium" color="white">Contact</Text>
+                    </NavLink>
+                  </Box>
+                </VStack>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+        </Box>
+      </Flex>
+      :
+      <Flex as="nav" alignItems="center" justify="space-between" h="10vh" w="100%" backgroundColor="#050A30">
+        {/* Logo */}
+        <NavLink to="/">
+          <Box padding="16px 24px">
+            <Text color="white">TuneHub</Text>
+          </Box>
+        </NavLink>
+
+        <HStack gap="24px" mr="40px">
+          {/* Home */}
+          <Box>
+            <NavLink to='/'>
+              <Text fontWeight="medium" color="white">Home</Text>
+            </NavLink>
+          </Box>
+          {/* Features */}
+          <Box>
+            <NavLink to='/'>
+              <Text fontWeight="medium" color="white">Features</Text>
+            </NavLink>
+          </Box>
+          {/* About Us */}
+          <Box>
+            <NavLink to='/'>
+              <Text fontWeight="medium" color="white">About Us</Text>
+            </NavLink>
+          </Box>
+          {/* News */}
+          <Box>
+            <NavLink to='/'>
+              <Text fontWeight="medium" color="white">News</Text>
+            </NavLink>
+          </Box>
+          {/* Contact */}
+          <Box>
+            <NavLink to='/'>
+              <Text fontWeight="medium" color="white">Contact</Text>
+            </NavLink>
+          </Box>
+          {/* CTA */}
           <NavLink to='/'>
-            <Text fontWeight="medium">Navbar</Text>
+            <Button fontWeight="medium" colorScheme="teal" variant="solid">Sign In/Up</Button>
           </NavLink>
-        </Box>
-        {/* Account avatar */}
-        <Box padding="16px 24px">
-          <Text>Profile</Text>
-        </Box>
-      </HStack>
-    </Flex>
+        </HStack>
+      </Flex>
   );
 }
 
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,13 +1,52 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { StarIcon } from '@chakra-ui/icons';
+import { Button, Flex, Text, Box } from '@chakra-ui/react';
 import React from 'react';
+import LandingPageSVG from '../../assets/landingpage.svg';
+import { useMediaQuery } from 'react-responsive';
 
 function LandingPage() {
+    const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
     return (
-        <Flex>
-            <Text>
-                Landing Page!
-            </Text>
-        </Flex>
+        isMobile ?
+            <Flex w="100%" minHeight="90vh" backgroundColor="#000C66" flexDir="column" alignItems="center" justifyContent="start">
+                <Flex mt="16px" w="100%" justifyContent="center">
+                    <img alt="TuneHub" src={LandingPageSVG} height="40%" width="80%" />
+                </Flex>
+                <Flex direction="column" gap="8px" mt="64px" mb="16px">
+                    <Flex alignItems="center">
+                        <StarIcon boxSize="4" color="white" mr="24px" />
+                        <Text fontSize="lg" fontWeight="medium" color="white">Song Recommendations</Text>
+                    </Flex>
+                    <Flex alignItems="center">
+                        <StarIcon boxSize="4" color="white" mr="24px" />
+                        <Text fontSize="lg" fontWeight="medium" color="white">News and Updates</Text>
+                    </Flex>
+                    <Flex alignItems="center">
+                        <StarIcon boxSize="4" color="white" mr="24px" />
+                        <Text fontSize="lg" fontWeight="medium" color="white">Spotify Integration</Text>
+                    </Flex>
+                </Flex>
+                <Button fontSize="md" fontWeight="medium" colorScheme='teal' mt="16px" height="32px">Get Started</Button>
+            </Flex>
+            :
+            <Flex w="100%" height="90vh" backgroundColor="#000C66" alignItems="center" justifyContent="space-evenly">
+                <Flex flexDir="column" gap="12px" width="35%">
+                    <Flex alignItems="center">
+                        <StarIcon boxSize="8" color="white" mr="24px" />
+                        <Text fontSize="4xl" fontWeight="medium" color="white">Song Recommendations</Text>
+                    </Flex>
+                    <Flex alignItems="center">
+                        <StarIcon boxSize="8" color="white" mr="24px" />
+                        <Text fontSize="4xl" fontWeight="medium" color="white">News and Updates</Text>
+                    </Flex>
+                    <Flex alignItems="center">
+                        <StarIcon boxSize="8" color="white" mr="24px" />
+                        <Text fontSize="4xl" fontWeight="medium" color="white">Spotify Integration</Text>
+                    </Flex>
+                    <Button fontSize="2xl" fontWeight="medium" colorScheme='teal' mt="16px" height="48px">Get Started</Button>
+                </Flex>
+                <img alt="TuneHub" src={LandingPageSVG} width="50%" />
+            </Flex>
     );
 }
 
