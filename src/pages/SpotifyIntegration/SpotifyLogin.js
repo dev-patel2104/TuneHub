2b12a9362b777f25650dd8569d9e6865cb3ac2cc
Harsh Pranav Patel <hr979846@dal.ca>
2023-07-25 01:34:55 -0300

    build fix

--- src/pages/SpotifyIntegration/SpotifyLogin.js
+++ src/pages/SpotifyIntegration/SpotifyLogin.js
@@ -8,7 +8,7 @@ function SpotifyLogin() {
     return (
         <Flex w="100%" minH="90vh" backgroundColor="#000C66" flexDirection="column" alignItems="center">
             <Text fontSize="4xl" fontWeight="medium" color="white" mt="24px" mb="128px">My Spotify</Text>
-            <img src={SpotifyLogo} />
+            <img alt="spotify" src={SpotifyLogo} />
             <Text fontSize="xl" fontWeight="medium" color="white" mt="24px">Connect with Spotify to unlock your Spotify Dashboard</Text>
             <Button variant="solid" colorScheme='teal' mt="32px">
                 <a href="https://tunehub-server.onrender.com/spotify/login">Login to Spotify</a>
--- src/pages/Trivia/quiz.js
+++ src/pages/Trivia/quiz.js
@@ -7,24 +7,24 @@ import quizImage1 from '../../assets/icon_insta_white.svg';
 import quizImage2 from '../../assets/icon_twitter_white.svg';
 
 function Quiz() {
-    const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
+    // const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
 
     // Sample quiz questions and options
-    const quizQuestions = [
-        {
-            question: "What is the capital of France?",
-            options: ["London", "Paris", "Berlin", "Rome"],
-            answer: "Paris",
-            image: quizImage1,
-        },
-        {
-            question: "Which planet is closest to the sun?",
-            options: ["Venus", "Mars", "Mercury", "Jupiter"],
-            answer: "Mercury",
-            image: quizImage2,
-        },
-        // Add more quiz questions here
-    ];
+    // const quizQuestions = [
+    //     {
+    //         question: "What is the capital of France?",
+    //         options: ["London", "Paris", "Berlin", "Rome"],
+    //         answer: "Paris",
+    //         image: quizImage1,
+    //     },
+    //     {
+    //         question: "Which planet is closest to the sun?",
+    //         options: ["Venus", "Mars", "Mercury", "Jupiter"],
+    //         answer: "Mercury",
+    //         image: quizImage2,
+    //     },
+    //     // Add more quiz questions here
+    // ];
 
     return (
         <Flex justifyContent="center" alignItems="center" minH="100vh" backgroundColor="#000C66" direction="column">
