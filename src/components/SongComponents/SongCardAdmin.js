3cd5494a92b4bd7c5b758f90cb82e4b1cb146286
Harsh Pranav Patel <hr979846@dal.ca>
2023-07-28 12:39:33 -0300

    delete fix

--- src/components/SongComponents/SongCardAdmin.js
+++ src/components/SongComponents/SongCardAdmin.js
@@ -108,7 +108,7 @@ function SongOverviewBox(props) {
             {duration}
           </Text>
           {/* DeleteIcon with confirmation dialog */}
-          <Icon as={DeleteIcon} _hover={{ bg: '#050A30', color: 'red.900' }} ml="4px" color="red.500" boxSize={responsiveIcon} onClick={() => setIsAlertOpen(true)} />
+          <Icon as={DeleteIcon} _hover={{ bg: '#050A30', color: 'red.900' }} ml="4px" color="red.500" boxSize={responsiveIcon} onMouseDown={() => setIsAlertOpen(true)} />
         </Flex>
       </Flex>
       </NavLink>
